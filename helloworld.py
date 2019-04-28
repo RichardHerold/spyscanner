8e9f5a2512662cadcf64e5e54563d9dcfc353405
Richard Herold <Richardherold@gmail.com>
2019-04-28 09:15:42 -0400

    again

--- helloworld.py
+++ helloworld.py
@@ -5,6 +5,6 @@ inky_display.set_border(inky_display.WHITE)
 
 from PIL import Image, ImageFont, ImageDraw
 
-img = Image.open("/home/pi/AGENTCONFIRMED.png")
+img = Image.open("resources/fingerprint.png")
 inky_display.set_image(img)
 inky_display.show()
