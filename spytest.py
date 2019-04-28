a7132c0727c9f825f2f3b00656a6584d31907920
Richard Herold <Richardherold@gmail.com>
2019-04-28 12:32:24 -0400

    tryit

---
+++ spytest.py
@@ -0,0 +1,15 @@
+from inky import InkyPHAT
+
+inky_display = InkyPHAT("yellow")
+inky_display.set_border(inky_display.WHITE)
+
+from PIL import Image, ImageFont, ImageDraw
+from font_fredoka_one import FredokaOne
+font = ImageFont.truetype(Courier, 11)
+
+img = Image.open("resources/fingerprint.png")
+draw = ImageDraw.Draw(img)
+
+draw.text((90, 20), "AGENT CONFIRMED", inky_display.WHITE, font=font)
+inky_display.set_image(img)
+inky_display.show()
