470025c4ed8dc3a25325890efba49f4cdb932c9b
Richard Herold <Richardherold@gmail.com>
2019-04-28 09:33:42 -0400

    fixed?

--- helloworld.py
+++ helloworld.py
@@ -4,22 +4,12 @@ inky_display = InkyPHAT("yellow")
 inky_display.set_border(inky_display.WHITE)
 
 from PIL import Image, ImageFont, ImageDraw
-
-img = Image.open("resources/fingerprint.png")
-inky_display.set_image(img)
-inky_display.show()
-
 from font_fredoka_one import FredokaOne
-
 font = ImageFont.truetype(FredokaOne, 12)
 
-from font_fredoka_one import FredokaOne
+img = Image.open("resources/fingerprint.png")
+draw = ImageDraw.Draw(img)
 
-message = "Agent Confirmed"
-w, h = font.getsize(message)
-x = (inky_display.WIDTH / 106) - (w / 2)
-y = (inky_display.HEIGHT / 2) - (h / 2)
-
-draw.text((x, y), message, inky_display.RED, font)
+draw.text((106, 2), "agent confirmed", inky_display.WHITE, font=font)
 inky_display.set_image(img)
 inky_display.show()
