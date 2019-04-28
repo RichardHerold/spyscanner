11b305c8a1c06dc835656fef34d2bbc034b09fb1
Richard Herold <Richardherold@gmail.com>
2019-04-28 09:05:12 -0400

    CONFIRMED

--- helloworld.py
+++ helloworld.py
@@ -5,18 +5,6 @@ inky_display.set_border(inky_display.WHITE)
 
 from PIL import Image, ImageFont, ImageDraw
 
-img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
-draw = ImageDraw.Draw(img)
-
-from font_fredoka_one import FredokaOne
-
-font = ImageFont.truetype(FredokaOne, 22)
-
-message = "Hello, World!"
-w, h = font.getsize(message)
-x = (inky_display.WIDTH / 2) - (w / 2)
-y = (inky_display.HEIGHT / 2) - (h / 2)
-
-draw.text((x, y), message, inky_display.RED, font)
+img = Image.open("/home/pi/AGENTCONFIRMED.png")
 inky_display.set_image(img)
 inky_display.show()
